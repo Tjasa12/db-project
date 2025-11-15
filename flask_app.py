703eff4c8830eb7b9274cf2cfb9403ea65cf8a3b
Luca Egli <luca.egli@mng.ch>
2025-11-15 12:06:06 +0100

    Check webhook secret before updating.

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,8 @@ from mysql.connector import pooling
 from dotenv import load_dotenv
 import os
 import git
+import hmac
+import hashlib
 
 load_dotenv()
 
@@ -12,6 +14,7 @@ DB_CONFIG = {
     "password": os.getenv("DB_PASSWORD"),
     "database": os.getenv("DB_DATABASE")
 }
+W_SECRET = os.getenv("W_SECRET")
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -21,13 +24,22 @@ pool = pooling.MySQLConnectionPool(pool_name="pool", pool_size=5, **DB_CONFIG)
 def get_conn():
     return pool.get_connection()
 
+def is_valid_signature(x_hub_signature, data, private_key):
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
 
 @app.post('/update_server')
 def webhook():
-    repo = git.Repo('./mysite')
-    origin = repo.remotes.origin
-    origin.pull()
-    return 'Updated PythonAnywhere successfully', 200
+    x_hub_signature = request.headers.get('X-Hub-Signature')
+    if is_valid_signature(x_hub_signature, request.data, W_SECRET):
+        repo = git.Repo('./mysite')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    return 'Unathorized', 401
 
 @app.route("/", methods=["GET", "POST"])
 def index():
